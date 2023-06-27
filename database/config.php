10f958d0c5512c58c5e6fc3bc70eae1d5f6527a5
BIpin <bipin.regmi.s21@gmail.com>
2023-06-27 23:55:54 +0545

    Bipin Acharya

---
+++ database/config.php
@@ -0,0 +1,6 @@
+<?php
+// Database Configuration
+$host = "localhost"; // Change this if your database host is different
+$db_name = "doc_laravel";
+$username = "root";
+$password = "";
---
+++ global.php
@@ -0,0 +1,4 @@
+<?php
+
+$base = 'http://doc_laravel.test';
+$img_base = 'http://doc_laravel.test/uploads/';
